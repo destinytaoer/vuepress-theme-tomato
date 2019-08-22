968bfdbb58bba6b449b558cd46abfe5940f936b1
destinytaoer <1848765519@qq.com>
2019-08-22 10:20:19 +0800

    modified theme config

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 module.exports = {
   plugins: [
-    ['@vuepress/blog', {
-      directories: [
-        {
-          id: 'post',
-          dirname: '_posts',
-          path: '/',
-          pagination: {
-            perPagePosts: 2,
-          },
-        },
-      ],
-      frontmatters: [
-        {
-          id: "tag",
-          keys: ['tag', 'tags'],
-          path: '/tag/',
-          layout: 'Tag',
-          frontmatter: { title: 'Tag' },
-          itemlayout: 'Tag',
-          pagination: {
-            perPagePosts: 3
+    [
+      '@vuepress/blog',
+      {
+        directories: [
+          {
+            id: 'post',
+            dirname: '_posts',
+            path: '/',
+            layout: 'Blog',
+            itemLayout: 'Post',
+            itemPermalink: '/:year/:month/:slug',
+            pagination: {
+              perPagePosts: 2
+            }
           }
-        },
-      ]
-    }],
-  ],
-}
+        ],
+        frontmatters: [
+          {
+            id: 'tag',
+            keys: ['tag', 'tags'],
+            path: '/tag/',
+            layout: 'Tag',
+            frontmatter: { title: 'Tag' },
+            itemlayout: 'Tag',
+            pagination: {
+              perPagePosts: 3
+            }
+          }
+        ]
+      }
+    ]
+  ]
+};
