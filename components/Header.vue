4323a3a14015796025a98390e1b00bf0a06bfd5b
destinytaoer <1848765519@qq.com>
2019-08-22 13:51:15 +0800

    增加 Header 组件

---
+++ components/Header.vue
@@ -0,0 +1,84 @@
+<template>
+  <header
+    class="header"
+    :class="{active}"
+  >
+    <h1 v-show="active">{{$site.title}}</h1>
+    <SearchBox v-if="$themeConfig.search !== false" />
+  </header>
+</template>
+<script>
+import SearchBox from "@vuepress/plugin-search/SearchBox";
+
+export default {
+  components: {
+    SearchBox
+  },
+  data() {
+    return {
+      active: false
+    };
+  },
+  methods: {
+    showHeader(e) {
+      this.active = !(this.getScrollTop() === 0);
+    },
+    getScrollTop() {
+      return (
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop ||
+        0
+      );
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.showHeader);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.showHeader);
+  }
+};
+</script>
+<style lang="stylus">
+@import '../styles/variables.styl';
+
+.header {
+  position: fixed;
+  width: 100%;
+  padding-left: $sidebarWidth;
+  margin-left: - $sidebarWidth;
+  height: 3em;
+  line-height: 3em;
+  text-align: center;
+  transition: all 0.3s linear;
+
+  &.active {
+    background-color: $themeColor;
+  }
+
+  h1 {
+    display: block;
+    margin: 0;
+    color: #fff;
+  }
+
+  .search-box {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    input:focus {
+      border-color: $accentColor;
+    }
+
+    .suggestion {
+      &.focused {
+        a {
+          color: $accentColor;
+        }
+      }
+    }
+  }
+}
+</style>
--- index.js
+++ index.js
@@ -39,6 +39,7 @@ module.exports = {
           }
         ]
       }
-    ]
+    ],
+    '@vuepress/search'
   ]
 };
