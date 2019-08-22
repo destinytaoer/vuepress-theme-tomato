4aeaeeab915a7ea2286f9ae512150679485e8596
destinytaoer <1848765519@qq.com>
2019-08-22 13:52:00 +0800

    [首页布局] 1. 增加侧边栏和头部位置，2. 调整样式

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -1,23 +1,55 @@
 <template>
-  <div id="global-layout">
-    <header style="background-color: #DDD">
-      <router-link to="/">{{ $site.title }}</router-link>· <router-link to="/tag/">Tag</router-link>
-    </header><br>
-    <DefaultGlobalLayout /><br>
-    <Footer></Footer>
+  <div
+    id="global-layout"
+    class="wrapper"
+  >
+    <div class="sidebar-wrapper">
+
+    </div>
+    <div class="content-wrapper">
+      <Header></Header>
+      <div class="content">
+        <DefaultGlobalLayout />
+      </div>
+      <Footer></Footer>
+    </div>
   </div>
 </template>
 
 <script>
 import GlobalLayout from "@app/components/GlobalLayout.vue";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default {
   components: {
     DefaultGlobalLayout: GlobalLayout,
+    Header,
     Footer
   }
 };
 </script>
 <style src="prismjs/themes/prism-tomorrow.css"></style>
 <style src="../styles/theme.styl" lang="stylus"></style>
+<style lang="stylus">
+@import '../styles/variables.styl';
+
+.wrapper {
+  display: flex;
+
+  .sidebar-wrapper {
+    flex: 0 $sidebarWidth;
+  }
+
+  .content-wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+
+    .content {
+      flex: 1;
+    }
+  }
+}
+</style>
